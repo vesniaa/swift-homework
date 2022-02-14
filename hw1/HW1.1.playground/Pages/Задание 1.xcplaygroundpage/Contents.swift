cf37537b1da7005a087ccba49ace3eea29507f77
Eugenia <pyssiki@yandex.ru>
2022-02-14 20:07:04 +0500

    hw1

---
+++ hw1/HW1.1.playground/Pages/Задание 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+
+import Foundation
+
+
+/*:
+ # Home Work 1
+ 
+ ## Задание 1
+ 
+ Объявите константу `firstString` со значением "I can" и `secondString` со значением "code". В значениях констант ни каких хитростей с лишними пробелами быть не должно. Выведите на консоль фразу **"I can code!"** с восклицательным знаком, используя эти строковые свойства.
+ */
+let firstString = "I can"
+let secondString = "code"
+
+print(firstString + " " + secondString + "!")
+
+print("\(firstString) \(secondString)!")
+//: Задание 1 из 3  |  [Далее: Задание 2](@next)
